d01921d652adf135601ef5b21698cad88ca0dccb
Juan Mosquella <jmosquella11@gmail.com>
2022-12-29 23:50:51 -0300

    cambios en nodemailer

--- config/nodeMailer.js
+++ config/nodeMailer.js
@@ -3,8 +3,7 @@ console.log(process.env.GMAIL_SECRET);
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
-  port: 587,
-  secure: false,
+
   auth: {
     user: process.env.GMAIL_SECRET,
     pass: process.env.GOOGLE_SECRET,
