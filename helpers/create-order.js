66557a21f60cf2729afee630f111d24b64aa80f6
Juan Mosquella <jmosquella11@gmail.com>
2023-01-07 18:51:26 -0300

    actualizada ruta

--- helpers/create-order.js
+++ helpers/create-order.js
@@ -1,9 +1,24 @@
-const createMPOrder = async (order) => {
-  const { paid_amount, items, collector } = order.response;
+const createMPOrder = async (order, user) => {
+  const { paid_amount, items, collector, order_status } = order.response;
 
   console.log(collector);
   console.log(paid_amount);
   console.log(items);
+  try {
+    const newOrder = {
+      name: collector.nickname,
+      products: items,
+      paymentMethod: "Mercado Pago",
+      orderStatus: order_status,
+      orderBy: user.id
+
+
+
+    }
+    
+  } catch (error) {
+    
+  }
 };
 
 export default createMPOrder;
